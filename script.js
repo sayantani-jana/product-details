893702fe154e36e42a56ba88455a923945ce3d51
Sayantani Jana <40852877+sayantani-jana@users.noreply.github.com>
2022-06-26 22:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var ProductImg=document.getElementById("ProductImg");
+var SmallImg=Document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick=function()
+{
+    ProductImg.src=SmallImg[0].src;
+}
+SmallImg[1].onclick=function()
+{
+    ProductImg.src=SmallImg[1].src;
+}
+SmallImg[2].onclick=function()
+{
+    ProductImg.src=SmallImg[2].src;
+}
+SmallImg[3].onclick=function()
+{
+    ProductImg.src=SmallImg[3].src;
+}
+SmallImg[4].onclick=function()
+{
+    ProductImg.src=SmallImg[4].src;
+}
